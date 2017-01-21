bdb2998cf999bc4133e6ef7d6e7cf92c73a0a552
Mark Monteiro <mark.monteiro23@gmail.com>
2017-01-21 15:03:37 -0500

    Add some debug output to packaging script

--- package.rb
+++ package.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'zip'
 
-# Delete existing file
+# Delete existing archive
 folder = Dir.pwd
-zipfile_name = folder + "/drumpfinator.zip"
+zipfile_name = folder + '/drumpfinator.zip'
 File.delete(zipfile_name) if File.exist?(zipfile_name)
 
 # Get files to zip
@@ -12,8 +12,11 @@ input_files.delete(File.basename(__FILE__))
 input_files.delete('README.md')
 
 # Create new archive
+puts 'Packaging extension into file ' + zipfile_name
 Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
   input_files.each do |filename|
+    print 'Adding file ' + filename + '...'
     zipfile.add(filename, folder + '/' + filename)
+    print "Done\n"
   end
 end
